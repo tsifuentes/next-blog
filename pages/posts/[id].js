6f3331c4a91b936384c3d218a6cd8d2552ba72ac
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-10 22:24:27 -0500

    feat: add page title and formatted date with date-fns

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,18 @@
+import Head from 'next/head'
+import Date from '../../components/date'
 import Layout from '../../components/layout'
 
 export default function Post({ postData }) {
     return (
       <Layout>
+          <Head>
+              <title>{postData.title}</title>
+          </Head>
         {postData.title}
         <br />
         {postData.id}
         <br />
-        {postData.date}
+        <Date dateString={postData.date}/>
         <br />
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
