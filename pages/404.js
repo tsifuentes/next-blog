cf61428c3ae844decfe73d5238838ce016dd580d
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-11 22:01:34 -0500

    feat: adding a nice 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1>Coming soon!</h1>
+            <p>Oops, this page is not available for now.</p>
+        </Layout>
+    )
+}
