1486ea8864680641d665f8f511c714e511e21341
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-20 23:18:44 -0500

    feat: add base for topic posts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Date from '../components/date'
 
-export default function Home({allPostsData, allDirectories}) {
+export default function Home({allDirectories}) {
   return (
     <Layout home>
       <Head>
@@ -13,41 +11,23 @@ export default function Home({allPostsData, allDirectories}) {
       <section className={utilStyles.headingMd}>
         <p>I am a Systems Engineer who loves Web Technologies, Algotithms, Software Architecture, Design Patterns, Apps, Cloud and more!</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}><a>{title}</a></Link>
-              <br />
-              {id}
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-
+        <h2 className={utilStyles.headingLg}>Topics</h2>
       <div className={utilStyles.flex}>
-      {allDirectories.map(item => (
-        <Card title={item}></Card>
-      ))}
+        {allDirectories.map(item => (
+          <Card title={item}></Card>
+        ))}
       </div>
     </Layout>
   )
 }
 
-import {getAllDirectories, getSortedPostsData} from '../lib/posts'
+import {getAllDirectories} from '../lib/posts'
 import Card from '../components/card'
 
 export async function getServerSideProps(context) {
-  const allPostsData = getSortedPostsData()
   const allDirectories = getAllDirectories();
   return {
     props: {
-      allPostsData,
       allDirectories
     }
   }
---
+++ pages/topic.js
@@ -0,0 +1,35 @@
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Topic({allPostsData}){
+    return (
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}><a>{title}</a></Link>
+              <br />
+              {id}
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+}
+
+import {getSortedPostsData} from '../lib/posts'
+
+export async function getServerSideProps(context) {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
